4222ba7c28bb5982abc6bea26d82a1d03293d021
Unknown <Domenico.Iapello@gmail.com>
2017-11-22 22:16:33 +0100

    antenna helper application not implemented

---
+++ ndnSimV24/ns-3/src/ndnSIM/apps/ndn-antenna-helper-app.cpp
@@ -0,0 +1,10 @@
+/*
+ * ndn-antenna-helper-app.cpp
+ *
+ *  Created on: Nov 22, 2017
+ *      Author: domenico
+ */
+
+
+
+
---
+++ ndnSimV24/ns-3/src/ndnSIM/apps/ndn-antenna-helper-app.hpp
@@ -0,0 +1,15 @@
+/*
+ * ndn-antenna-helper-app.hpp
+ *
+ *  Created on: Nov 22, 2017
+ *      Author: domenico
+ */
+
+#ifndef NDN_ANTENNA_HELPER_APP_HPP
+#define NNDN_ANTENNA_HELPER_APP_HPP
+
+
+
+
+
+#endif /* NDN_ANTENNA_HELPER_APP_HPP*/
